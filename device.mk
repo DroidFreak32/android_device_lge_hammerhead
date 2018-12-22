8e397d8f07e8788a7b54bbd7d92e95378b37af15
Bruno Martins <bgcngm@gmail.com>
2018-12-22 18:58:43 +0000

    hammerhead: Enable memory optimizations
     * Enable B-services aging propagation and delay service restart
       for devices with 2GB or less memory.
    
    Change-Id: I81a09cc8425602bfc586875b7d36a4f56a226908

--- device.mk
+++ device.mk
@@ -393,3 +393,8 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2016-10-05
+
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.vendor.qti.sys.fw.bservice_enable=true
